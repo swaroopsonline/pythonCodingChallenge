15f9bbf01e776c2fc8ee1e0ba3412530e6d95423
swaroopsonline <swaroopsonline@gmail.com>
2023-04-05 23:47:38 +1000

    5th April optimization

--- CheckTwoArraysForEquality.py
+++ CheckTwoArraysForEquality.py
@@ -7,7 +7,8 @@ import numpy as np
 # of elements may be different though.
 # Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.
 
-
+# Note: In the below example an Array is declared. However, an array will always consider its elements of the same
+# DataType.
 # arr1 = np.array(["Hyundai", 1, "Volvo", "Maruti", False])
 arr1 = np.array([2, "Logan", "Fiat", True, "Volvo", 3])
 
@@ -20,18 +21,16 @@ listarr2 = list(arr2)
 
 # Converting both the Arrays to Set to identify the Unique elements
 set1 = set(listarr1)
-# print(len(set1))
 listset1 = list(set1)
-# print(len(listset1))
 
 set2 = set(listarr2)
-# print(len(set2))
 listset2 = list(set2)
-# print(len(listset2))
 
+# Declaring an empty list by the name of listarr to extract the common elements of both the Arrays.
 listarr = list()
 
 
+# Creating a Function to extract the common elements from both the Arrays
 def common_elements():
     for i in range(len(listset1)):
         for j in range(len(listset2)):
@@ -47,8 +46,6 @@ elif len(listarr1) != len(listarr2):
     common_elements()
     print("Both the Arrays are not equal in size")
     print("Common elements for the varied sized arrays are", listarr)
-
-# Creating a Function to extract the common elements from both the Arrays
 
 if len(listarr) == len(listset1):
     if len(listarr) == len(listset2):
